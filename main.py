8876a6d5a24df62ff6b4770e3f3f6914e5d873c8
rukshanasafrin2909-sketch <rukshanasafrin2909@gmail.com>
2026-08-26 20:26:07 +0530

    First program in python

---
+++ main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Rukshana.")
+
+# Printing a fun fact about myself
+print("I love playing Badminton.")
+
+# Performing and printing a simple calculation
+print("The result of 2 + 7 is:", 2 + 7)
